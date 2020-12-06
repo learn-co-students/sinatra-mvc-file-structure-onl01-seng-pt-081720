130ba300f7c44aec7f7bfcae393772610cae310e
Janaeq <janaemassa@gmail.com>
2020-12-05 22:19:52 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,15 +1,18 @@
 require 'pry'
 class Dog
     attr_accessor :name, :breed, :age
-    @@all = []
+
+    DOGS = []
+
     def initialize(name, breed, age)
         @name = name
         @breed = breed
         @age = age
-        @@all << self
+        DOGS << self
     end
 
     def self.all
-        @@all
+        DOGS
     end
+    
 end
